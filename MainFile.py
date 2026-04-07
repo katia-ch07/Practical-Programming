aae66b0f801e97b429786b904523ce16b5666717
katia-ch07 <chalalkatia22@gmail.com>
2026-04-07 22:16:18 +0100

    Add JSON file handling for saving and loading data

--- MainFile.py
+++ MainFile.py
@@ -2,6 +2,19 @@
 
 #first import libraries 
 import tkinter as tk  #GUI creation (python software foundation, 2024)
+import json #JSON handling (Python Software Foundation, 2024)
+import os #file system (Programiz, 2024)
+
+DATA_FILE = "students.json"
+
+#load data
+def load_data():
+    #we have to check if the file exists first (Real python, 2023)
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE):
+            with open(DATA_FILE, "r") as file:
+                return json.load(file)
+    return {}
 
 #this is main app class 
 class App(tk.Tk):
@@ -13,10 +26,10 @@ class App(tk.Tk):
         self.geometry("800x600")
 
         #simple title label
-        title = tk.Label(self, text="Student Tracker", font=("Arial", 18))
-        title.pack(pady=20)
+        title = tk.Label(self, text="Student Tracker", font=("Arial", 18)).pack(pady=20)
 
 #run program 
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+    
